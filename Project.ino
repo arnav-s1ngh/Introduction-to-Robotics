d015cf4d80ef0408ce6ff86d8af0dbaed7982483
arnav-s1ngh <92050142+arnav-s1ngh@users.noreply.github.com>
2024-10-21 20:08:18 +0530

    Update Project.ino

--- Project.ino
+++ Project.ino
@@ -1,14 +1,10 @@
-
-//including the libraries
 #include <AFMotor.h>
 
-// Defining pins and variables
 #define lefts A1
 #define rights A0
 
-#define baseSpeed 200  // Base speed for motors
+#define speed 255
 
-// Defining motors
 AF_DCMotor motor1(1, MOTOR12_1KHZ); 
 AF_DCMotor motor2(2, MOTOR12_1KHZ);
 AF_DCMotor motor3(3, MOTOR34_1KHZ);
