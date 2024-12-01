447b02e899f25221e6a5ea63aea1459a80c426df
arnav-s1ngh <92050142+arnav-s1ngh@users.noreply.github.com>
2024-12-01 16:43:22 +0530

    Update code.m

--- Assignment-2/code.m
+++ Assignment-2/code.m
@@ -1,30 +1,82 @@
-% Robotics Assignment-2
-
-% Initial Parameters
-x_i=0;
-y_i=0;
-theta_i=0;
+% Pure Pursuit Controller for Unicycle Robot
+% References:- https://arxiv.org/html/2409.09848v1#:~:text=The%20pure%20pursuit%20method%20stands,and%20the%20vehicle's%20reference%20point.
+% References:- https://in.mathworks.com/matlabcentral/answers/1696610-pure-pursuit-controller-should-follow-y-x-5
+close all;
+clear;
+clc;
+% Initial Pose
+x=0;             
+y=0;            
+theta=0;     
+dt=0.0005;       
+T=15;            
 v=2;
-r=1;
-t=15;
-dt=0.01;
-d=0.01; %look ahead distance
-kv=1;
-ki=1;
+% Parameters
+ld=0.008;
+radius=1;      
+dur=0:dt:T;
+s=size(dur);
+xpath=zeros(s);
+ypath=zeros(s);
+thetapath=zeros(s);
+% Radial Distance
+errorpath=zeros(s);
 
-% Desired Trajectory
-theta_f=(v/r)*tim;
-x_f=r*cos(theta_f);
-y_f=r*sin(theta_f);
-
-
-for i= 0:dt:t
-    x_error=x_f-x_i;
-    y_error=y_f-y_i;
-    net_error=sqrt(x_error^2+y_error^2)-d;
-    v_i=kv*net_error
-
+for i=1:length(dur)
+    xpath(i)=x;
+    ypath(i)=y;
+    thetapath(i)=rem(theta,2*pi);
+    errorpath(i)=abs(sqrt(x^2+y^2)-radius);
+    current_angle=atan2(y,x);
+    ld_angle=current_angle+v*dt/radius;
+    x_ahead=radius*cos(ld_angle);
+    y_ahead=radius*sin(ld_angle);
+    dx=x_ahead-x;
+    dy=y_ahead-y;
+    x=x+v*cos(theta)*dt;
+    y=y+v*sin(theta)*dt;
+    alpha=atan2(dy,dx)-theta;
+    % The first formula is for a unicycle with wheel radius=1, while the
+    % second is for a point object, that i found on the internet
+    % phi=atan(2*(1/ld)*sin(alpha));
+    % using this formula for phi since it gives a good result
+    phi=2*v/ld*sin(alpha)*dt;
+    theta=theta+phi; 
 end
-    
 
+figure;
+plot(dur,errorpath,"b-");
+title("e-vs-t");
+xlabel("Error");
+ylabel("Time");
 
+figure;
+plot(dur,xpath,"b-");
+title("X-vs-t");
+xlabel("T");
+ylabel("X");
+
+figure;
+plot(dur,ypath,"b-");
+title("Y-vs-t");
+xlabel("t");
+ylabel("Y");
+
+figure;
+plot(dur,thetapath,"b-");
+title("θ-vs-t");
+xlabel("T");
+ylabel("θ");
+
+figure;
+plot(xpath,ypath,"b-","LineWidth",2);
+hold on;
+axis equal;
+grid on;
+plot(1,0,"ro");
+plot(0,1,"ro");
+plot(-1,0,"ro");
+plot(0,-1,"ro");
+title("Y-vs-X");
+xlabel("X");
+ylabel("Y");
